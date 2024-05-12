eca5562f7e0fcee13d723c64cff0529f30f32af5
mahathibodela <121395753+mahathibodela@users.noreply.github.com>
2024-05-12 21:50:41 +0530

    Time: 18 ms (93.89%) | Memory: 12 MB (82.74%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,24 +1,39 @@
-class Solution:
-    def binsearch(self,arr,target,l,r):
-        if l<=r:
-            lr = l//len(arr[0])
-            lc = l%len(arr[0])
-            ur = r//len(arr[0])
-            uc = r%len(arr[0])
-            if(arr[lr][lc]==target): return True;
-            if(arr[ur][uc]==target): return True
-            mid = l + (r-l)//2
-            ra = mid//len(arr[0])
-            c = mid%len(arr[0])
-            print(r,"and",c,"and",mid,"and",l,ra)
-            if arr[ra][c]==target:
-                print("here")
-                return True
-            elif arr[ra][c]>target:
-                return self.binsearch(arr,target,l,mid-1)
-            else:
-                return self.binsearch(arr,target,mid+1,r)
-        return False
-
+class Solution(object):
     def searchMatrix(self, matrix, target):
-        return self.binsearch(matrix,target,0,len(matrix)*len(matrix[0])-1)
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rowans = colans = -1
+
+        l = 0
+        h = len(matrix) - 1
+        n = h
+        m = len(matrix[0]) - 1
+
+        while(l <= h):
+            mid = (l + h) // 2
+
+            if(matrix[mid][m] >= target):
+                rowans = mid
+                h = mid - 1
+            else:
+                l = mid + 1
+        
+        l = 0
+        h = m 
+        print(rowans)
+        while(l<=h):
+            mid = (l + h) // 2
+            print(mid)
+
+            if(matrix[rowans][mid] == target):
+                return True
+            if(matrix[rowans][mid] < target):
+                l= mid + 1
+            else:
+                h = mid - 1
+        
+        return False
+        
