e9fb37c0f24310309e8714524e379d31ee45db35
mahathibodela <121395753+mahathibodela@users.noreply.github.com>
2024-06-21 10:33:24 +0530

    Time: 37 ms (40.00%) | Memory: 12 MB (37.07%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -11,18 +11,15 @@ class Solution(object):
 
         while r < n:
             if chars[r] != chars[l]:
-                if c == 1:
-                    l += 1
-                else:
+                if c != 1:
                     for i in (str(c)):
                         l += 1
                         chars[l] = i
-                    l += 1
+                l += 1
                 chars[l] = chars[r]
                 c = 0
             c += 1
             r += 1
-            print(l, r, c)
         
         if c == 1: return l + 1
         for i in (str(c)):
