f80ab1efed73fba3067b49ff8bdc97b703af0740
mahathibodela <121395753+mahathibodela@users.noreply.github.com>
2024-06-23 15:19:00 +0530

    Time: 15 ms (99.45%) | Memory: 13.3 MB (21.43%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -10,28 +10,34 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        if root == None: return []
-        q = []
-        q.append((root, 1))
+        if root == None:
+            return 0
+
+        q = [(root, 1)]
         ans = 0
 
-        while len(q) != 0:
+        while q:
             s = len(q)
-            f = l = 0
+            f = -1
+            e = -1
+
             for i in range(s):
-                node, no = q.pop(0)
+                node, c = q.pop(0)
                 if i == 0:
-                    f = no
-                if i == s - 1:
-                    l = no
+                    f = c
+                if i == s-1:
+                    e = c
                 
-                if node.left != None: q.append((node.left, 2 * no - f))
-                if node.right != None: q.append((node.right, 2 * no + 1 - f))
-            
-            print(l, f)
-            ans = max(ans, l - f + 1)
+                if node.left != None:
+                    q.append((node.left, 2 * c + 1 - f))
+                if node.right != None:
+                    q.append((node.right, 2 * c + 2 - f))
+
+            ans = max(ans, e - f + 1)
         
         return ans
+                    
 
-        
+
+
         
