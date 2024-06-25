b9280bb3db7d3c487a8deddc4003e076e0474063
mahathibodela <121395753+mahathibodela@users.noreply.github.com>
2024-06-25 08:24:36 +0530

    Time: 143 ms (96.46%) | Memory: 12.1 MB (83.19%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
@@ -4,17 +4,15 @@ class Solution(object):
         :type pairs: List[List[int]]
         :rtype: int
         """
+        
+        pairs.sort(key = lambda p: p[1])
+        pre = 0
+        c = 1
 
-        pairs.sort()
-        n = len(pairs)
-        dp = [1 for i in range(n)]
-        ans = 1
         for i in range(1, len(pairs)):
-            for j in range(i):
-                if pairs[j][1] < pairs[i][0] and dp[j] + 1 > dp[i]:
-                    dp[i] = dp[j] + 1
-            ans = max(ans, dp[i])
+            s, e = pairs[i]
+            if pairs[pre][1] < s:
+                c += 1
+                pre = i
         
-        return ans
-
-        
+        return c
