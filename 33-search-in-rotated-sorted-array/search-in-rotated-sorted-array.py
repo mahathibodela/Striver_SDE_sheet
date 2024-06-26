fcde18a113e1931c9347ba165460c7549c5c7f90
mahathibodela <121395753+mahathibodela@users.noreply.github.com>
2024-06-26 09:48:10 +0530

    Time: 18 ms (89.30%) | Memory: 11.7 MB (98.80%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,31 +5,28 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-
+        n = len(nums)
         l = 0
-        h = len(nums) - 1
-        n = len(nums) - 1
-        while(l <= h):
-            mid = (l + h) / 2
-            print(mid, l, h)
+        r = n - 1
+        if nums[0] == target: return 0
+        if nums[-1] == target: return n - 1
+        if r == 0: return -1
 
-            if(nums[mid] == target):
+        while l <= r:
+            mid = (l + r) / 2
+
+            if nums[mid] == target:
                 return mid
-
-            # left half is sorted
-            elif(nums[l] <= nums[mid]):
-                if(nums[l] <= target and target < nums[mid]):
-                    h = mid - 1
+            
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
                 else:
                     l = mid + 1
-
-            else:
-                if(nums[mid] < target and target <= nums[h]):
+            elif nums[r] >= nums[mid]:
+                if nums[mid] <= target <= nums[r]:
                     l = mid + 1
                 else:
-                    h = mid - 1
-
+                    r = mid - 1
+        
         return -1
-
-        
-        
