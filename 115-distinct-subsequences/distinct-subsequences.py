17a6696bc8e96cb823a3409ea32ae64e00793bf6
mahathibodela <121395753+mahathibodela@users.noreply.github.com>
2024-06-28 13:18:23 +0530

    Time: 35 ms (99.64%) | Memory: 13.7 MB (82.31%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -6,25 +6,25 @@ class Solution(object):
         :rtype: int
         """
 
+
         def check(i, j, dp):
             if j == m:
                 return 1
-            if i >= n:
-                return 0
-            if (i, j) in dp:
+            
+            if(i, j) in dp:
                 return dp[(i, j)]
             
-            tar = n - (m - (j)) + 1
             ans = 0
-            for k in range(i, tar):
+            for k in range(i, n - (m - j) + 1):
                 if s[k] == t[j]:
                     ans += check(k + 1, j + 1, dp)
- 
+            
             dp[(i, j)] = ans
             return ans
-            
-        dp = {}
+
         n = len(s)
         m = len(t)
+        dp = {}
+
         k = check(0, 0, dp)
         return k
