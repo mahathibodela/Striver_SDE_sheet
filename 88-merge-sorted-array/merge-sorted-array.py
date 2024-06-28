a20d9b8cf3b2ac328c879f6ad4aa40e29aa5fcf4
mahathibodela <121395753+mahathibodela@users.noreply.github.com>
2024-06-28 11:34:55 +0530

    Time: 25 ms (16.98%) | Memory: 11.6 MB (32.44%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,29 +7,22 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        i = 0
-        j = n - 1
-        for c in range(m, m + n):
-            nums1[c] = -(10 ** 10)
 
-        while i < m and j >= 0:
-            if nums2[j] > nums1[i]:
-                temp = nums1[i]
-                nums1[i] = nums2[j]
-                nums2[j] = temp
-                i += 1
-                j -= 1
-            else:
-                break
-        
-        nums1.sort()
-        nums2.sort()
-        print(nums1, nums2)
-        for i in range(n):
-            nums1[i] = nums2[i]
-        
-        return nums1
-        
-     
+        l = m + n - 1
+        m = m - 1
+        n -= 1
 
+        while m >= 0 and n >= 0:
+            if nums1[m] >= nums2[n]:
+                nums1[l] = nums1[m]
+                m -= 1
+            else: 
+                nums1[l] = nums2[n]
+                n -= 1
+            l -= 1
+            
+        while n >= 0:
+            nums1[l] = nums2[n]
+            n -= 1
+            l -= 1
         
